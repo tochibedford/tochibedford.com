2a96f9f46529434e787bb85609a145fe64768a76
tochibedford <musicofbyte@gmail.com>
2023-11-28 02:23:06 +0100

    custom colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        tBlack: "#232124",
+        tWhite: "#DEDEDE",
+        tWhiteBright: "#E5E5E5",
+        tWhiteBrighter: "#EEECEC",
+        tMenuDark: "#69636D",
+      },
+    },
+  },
+  plugins: [],
+};
